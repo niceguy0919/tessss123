d7f3dbaf2142af8abe14163661fcf57779116232
niceguy0919 <niceguy1648@yahoo.com>
2017-06-25 22:56:40 -0700

    Update vnplaylist.py

--- vnplaylist.py
+++ vnplaylist.py
@@ -12,8 +12,8 @@ addons_folder = xbmc.translatePath('special://home/addons')
 image         = xbmc.translatePath(os.path.join(path, "icon.png"))
 
 plugin         = Plugin()
-addon          = xbmcaddon.Addon("plugin.video.thongld.vnplaylist")
-pluginrootpath = "plugin://plugin.video.thongld.vnplaylist"
+addon          = xbmcaddon.Addon("plugin.video.Anime4all")
+pluginrootpath = "plugin://plugin.video.Anime4all"
 http           = httplib2.Http(cache, disable_ssl_certificate_validation=True)
 query_url      = "https://docs.google.com/spreadsheets/d/{sid}/gviz/tq?gid={gid}&headers=1&tq={tq}"
 sheet_headers  = {
@@ -612,7 +612,7 @@ def AddTracking(items):
 	'''
 
 	for item in items:
-		if "plugin.video.thongld.vnplaylist" in item["path"]:
+		if "plugin.video.Anime4all" in item["path"]:
 			tmps = item["path"].split("?")
 			if len(tmps) == 1:
 				tail = ""
